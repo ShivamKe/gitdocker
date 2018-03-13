e8ab62afd9e5b5350798c7648b1467674e040cc2
root <root@ip-172-31-16-215.ap-south-1.compute.internal>
2018-03-13 14:29:49 +0530

    edit on 03-Mar

--- dockerstop.sh
+++ dockerstop.sh
@@ -4,6 +4,6 @@ docker ps -a | grep httpd-test
 if [ "$?" -eq 0 ]
  then
    echo "Container httpd-test already running, Stopping it..."
-   /bin/docker stop httpd-test && echo "Container httpd-test stopped.."
+   /bin/docker stop httpd-test && echo "Container httpd-test stopped.." && sleep 2
 fi
 
